d7cee66b1c0bfe66c1b93c0c5fd299fc1caca9b4
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2009-04-23 21:04:00 +0000

    попытка добавить папку

---
+++ osgSimpleGLSL/GL2Scene.h
@@ -0,0 +1,35 @@
+#ifndef _GL_2_SCENE_H_
+#define  _GL_2_SCENE_H_
+
+#include <osg/Node>
+#include <osg/Referenced>
+#include <osg/ref_ptr>
+
+#include <osg/Program>
+
+class GL2Scene : public osg::Referenced
+{
+public:
+	GL2Scene();
+
+	osg::ref_ptr< osg::Group > getRootNode() { return _rootNode; }
+	
+protected:
+	~GL2Scene();
+
+private:	/*methods*/
+	osg::ref_ptr<osg::Group> buildScene();
+
+private:	/*data*/
+	//добавить шейдер в сцену
+	void AddShader();
+
+	// load source from a file.
+	void LoadShaderSource( osg::Shader* shader, const std::string& fileName );
+
+	osg::ref_ptr<osg::Group> _rootNode;
+};
+
+typedef osg::ref_ptr<GL2Scene> GL2ScenePtr;
+
+#endif	//_GL_2_SCENE_H_
---
+++ osgSimpleGLSL/GL2SceneCallback.cpp
@@ -0,0 +1,42 @@
+#include "GL2SceneCallback.h"
+
+#include <osg/Group>
+#include <osg/Uniform>
+
+#include <iostream>
+GL2SceneCallback::GL2SceneCallback()
+{
+
+}
+
+GL2SceneCallback::~GL2SceneCallback()
+{
+	std::cout << "~GL2SceneCallback()\n";
+}
+
+void GL2SceneCallback::operator()( osg::Node* node, osg::NodeVisitor* nv )
+{
+	osg::Group *group = dynamic_cast< osg::Group* >( node );
+	if ( group )
+	{
+		//получить состояние узла
+		osg::StateSet* ss = group->getOrCreateStateSet();
+
+		//извлечь uniform
+		osg::Uniform *_color = ss->getUniform( "_ZZ3SconstantColor" );
+
+		if ( _color )
+			//задать случайный цвет
+			SetRandColor( _color );
+	}
+}
+
+void GL2SceneCallback::SetRandColor( osg::Uniform *_color )
+{
+	//задать случайный цвет
+	float r = (float)rand() / (float)RAND_MAX;
+	float g = (float)rand() / (float)RAND_MAX;
+	float b = (float)rand() / (float)RAND_MAX;
+
+	_color->set( osg::Vec3( r , g , b ) );
+}
