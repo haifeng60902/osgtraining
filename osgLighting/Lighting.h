626713822ed0724d9d1c0cd7f1a748c3d2775cfc
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2009-07-15 21:46:34 +0000

    работа с освещением

---
+++ osgLighting/Lighting.h
@@ -0,0 +1,29 @@
+#ifndef _LIGHTING_H_
+#define _LIGHTING_H_
+
+#include <osg/Group>
+#include <osg/Referenced>
+#include <osg/ref_ptr>
+
+class Lighting : public osg::Referenced
+{
+public:
+	Lighting();
+	virtual ~Lighting();
+
+	osg::ref_ptr< osg::Group > getRootNode() { return m_rootGroup.get(); }
+
+private:
+	/*methods*/
+	void buildScene();
+
+	//загрузить модель
+	osg::ref_ptr< osg::Node > LoadModel();
+
+	//загрузить модель
+	osg::ref_ptr< osg::Node >CreatePlane();
+
+	osg::ref_ptr< osg::Group > m_rootGroup;
+};
+
+#endif	//_LIGHTING_H_
