d4a586caacf5341cbbff392065e7d669e6b5c1b6
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2009-07-16 14:58:12 +0000

    работа с освещением

--- osgLighting/Lighting.h
+++ osgLighting/Lighting.h
@@ -4,6 +4,9 @@
 #include <osg/Group>
 #include <osg/Referenced>
 #include <osg/ref_ptr>
+#include <osg/Transform>
+
+#include <vector>
 
 class Lighting : public osg::Referenced
 {
@@ -20,10 +23,18 @@ private:
 	//загрузить модель
 	osg::ref_ptr< osg::Node > LoadModel();
 
-	//загрузить модель
-	osg::ref_ptr< osg::Node >CreatePlane();
+	//создать плоскость
+	osg::ref_ptr< osg::Node > CreatePlane();
+
+	//создать точку представляющую источник света
+	osg::ref_ptr< osg::Geode > createLightPoint();
+
+	//создать источник света
+	osg::ref_ptr< osg::Transform > CreateLight();
 
 	osg::ref_ptr< osg::Group > m_rootGroup;
+
+	std::vector< unsigned int > m_vIndex;
 };
 
 #endif	//_LIGHTING_H_
