8a8d01f45841ea69a62bef78557fa91c91679ad7
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2009-07-27 21:09:01 +0000

    теперь точки пересечения находятся

--- LineIntersector/DynamicLinesCallback.h
+++ LineIntersector/DynamicLinesCallback.h
@@ -6,6 +6,8 @@
 #include <osg/Group>
 #include <osg/PrimitiveSet>
 
+#include <vector>
+
 class DynamicLinesCallback : public osg::NodeCallback
 {
 public:
@@ -19,23 +21,37 @@ private:
 	//инициализация узла, содержащего точки
 	void InitPointsNode();
 	
+	//найти группу линий
+	osg::ref_ptr< osg::Group > GetLinesGroup( osg::Node* node );
+
 	//динамическое обновление положения линий
-	void DinamicUpdateLines( osg::Node* node );
+	void DinamicUpdateLines();
 
 	//определение колизий
-	void ColisionDetection( osg::Vec3Array* v , osg::Node* node );
+	void ColisionDetection();
 
 	//получить случайное число
 	float GetRand( float fScale );
 
+	//найти массив вершин
+	void FindVertexArray( osg::ref_ptr< osg::Group > group );
+
+	//заполнить узел новыми координатами точек
+	void FillPointsNode( const std::vector< osg::Vec3d > &res );
+
 	//узел содержащий точки пересечения геометрии линиями
 	osg::ref_ptr< osg::Group > m_PointsGroup;
+
+	//координаты вершин линий
+	osg::ref_ptr< osg::Vec3Array > m_LinesVertexs;
 
 	//данные для координат точек и цветов
 	osg::ref_ptr< osg::Vec3Array > m_V;
 	osg::ref_ptr< osg::Vec4Array > m_C;
 
 	osg::ref_ptr< osg::DrawArrays > m_DA;
+
+	osg::Node *m_pNode;
 };
 
 #endif	//_DYNAMIC_LINES_CALLBACK_H_
