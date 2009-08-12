772982b155afe10e37595f1da5d2a3e87690295b
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2009-08-12 14:30:18 +0000

    имитация леса

---
+++ Forest/BranchXML.h
@@ -0,0 +1,33 @@
+#ifndef _BRANCH_XML_H_
+#define _BRANCH_XML_H_
+
+#include <osg/Referenced>
+#include <osg/ref_ptr>
+#include <osg/Geode>
+
+class BranchXML : public osg::Referenced
+{
+public:
+	BranchXML();
+	~BranchXML();
+
+	osg::ref_ptr< osg::Geode > getBranchGeode() { return m_branchGeode.get(); }
+
+private:
+	//инициировать корневой узел данными
+	void InitBranchGeode();
+
+	//инициировать корневой узел данными со многими стволами
+	void InitManyBranchGeode();
+
+	//добавить текстуру
+	void AddTexture();
+
+	//настроить альфа канал
+	void SetupAlfaFunc();
+
+	//корневой узел геометрии ствола
+	osg::ref_ptr< osg::Geode > m_branchGeode;
+};
+
+#endif	//_BRANCH_XML_H_
---
+++ Forest/LightSource.h
@@ -0,0 +1,39 @@
+#ifndef _LIGHT_SOURCE_H_
+#define _LIGHT_SOURCE_H_
+
+#include "LightSourceCallback.h"
+
+#include <osg/Referenced>
+#include <osg/Group>
+#include <osg/ref_ptr>
+#include <osg/Transform>
+
+class LightSource : public osg::Referenced
+{
+public:
+	LightSource();
+	~LightSource();
+
+	//вернуть узел света
+	osg::ref_ptr< osg::Group > getRootNode() { return m_rootGroup.get(); }
+
+	//передать Uniform положения источника света
+	void SetUniform( osg::Uniform *_LightPos ){ cb->SetUniform( _LightPos ); };
+
+private:
+	/*methods*/
+	void buildScene();
+
+	//создать точку представляющую источник света
+	osg::ref_ptr< osg::Geode > createLightPoint();
+
+	//создать источник света
+	osg::ref_ptr< osg::Transform > CreateLight();
+
+	//группа, содержащая геометрию источника света и сам свет
+	osg::ref_ptr< osg::Group > m_rootGroup;
+
+	LightSourceCallback *cb;
+};
+
+#endif	//_LIGHT_SOURCE_H_
