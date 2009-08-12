9500ecac32738a72b00d1a99d04c939cd1e34c13
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2009-08-12 11:24:13 +0000

    в этом проекта будет строиться лес

---
+++ Forest/main.cpp
@@ -0,0 +1,24 @@
+//#include "xmlRoot/xmlRoot.h"
+
+//#include "Tree.h"
+
+#include <osgViewer/Viewer>
+#include <osgViewer/ViewerEventHandlers>
+#include <osgDB/ReadFile>
+#include <osgDB/WriteFile>
+
+int main()
+{
+//	xmlRoot::Instance().Init( "tree.xml" );
+
+	// Create a Viewer.
+	osgViewer::Viewer viewer;
+
+	// add the stats handler
+	viewer.addEventHandler( new osgViewer::StatsHandler );
+
+	viewer.getCamera()->setComputeNearFarMode(osg::CullSettings::DO_NOT_COMPUTE_NEAR_FAR); 
+
+	// Display, and main loop.
+	return viewer.run();
+}
