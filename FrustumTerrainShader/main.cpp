dcc045a36f7a8799d856bb2b8af4d96e5d10ae0e
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2009-09-25 10:47:14 +0000

    в этом проекте будет повторено отображение земли, но уже с помощью шейдеров

---
+++ FrustumTerrainShader/main.cpp
@@ -0,0 +1,29 @@
+//#include "TerrainNode.h"
+
+#include <osgViewer/Viewer>
+#include <osgViewer/ViewerEventHandlers>
+
+int main()
+{
+//	osg::ref_ptr< TerrainNode > terrain = new TerrainNode;
+
+	// Create a Viewer.
+	osgViewer::Viewer viewer;
+
+	// add the stats handler
+	viewer.addEventHandler( new osgViewer::StatsHandler );
+
+	//	viewer.addEventHandler( new KeyboardHandler );
+
+	//настройка камеры
+	viewer.getCamera()->setProjectionMatrixAsPerspective( 112.5, 1050.0 / 1680.0 , 1.0 , 30000.0 );
+
+//	viewer.setSceneData( terrain->getRootNode().get() );
+
+	viewer.getCamera()->setComputeNearFarMode(osg::CullSettings::DO_NOT_COMPUTE_NEAR_FAR); 
+
+	//viewer.setUpViewInWindow(40, 40, 640, 480 );
+
+	// Display, and main loop.
+	return viewer.run();
+}
