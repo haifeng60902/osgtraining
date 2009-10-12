200e89307abd34e3934fa0b35604ea59655c7c47
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2009-10-12 21:30:47 +0000

    +2 файла

---
+++ FrustumTerrainShader/BuildingShader/BuildingShaderPatchNode.cpp
@@ -0,0 +1,72 @@
+#include "BuildingShaderPatchNode.h"
+
+#include <osgDB/ReadFile>
+#include <osgDB/FileUtils>
+#include <osg/Image>
+#include <osg/Texture2D>
+
+BuildingShaderPatchNode::BuildingShaderPatchNode()
+{
+	m_rootNode = new osg::Group;
+
+	//инициализировать корневой узел застройки
+	InitBuildingNode();
+
+	//динамически мен€емый узел
+	m_rootNode->setDataVariance( osg::Object::DYNAMIC );
+}
+
+BuildingShaderPatchNode::~BuildingShaderPatchNode()
+{
+
+}
+
+void BuildingShaderPatchNode::InitBuildingNode()
+{
+	//инициализировать корневой узел застройки
+
+	//добавить шейдер
+	AddShader();
+
+	//задать обратный вызов обновлени€
+	//m_rootNode->setUpdateCallback( new DynamicGroupUpdateCallback( m_unfVisPos.get()) );
+}
+
+void BuildingShaderPatchNode::AddShader()
+{
+	//добавить шейдер
+
+	//формирование сцены с шейдером
+	osg::StateSet* ss = m_rootNode->getOrCreateStateSet();
+
+	//создать экземпл€р программы
+	osg::Program* program = new osg::Program;
+	program->setName( "building_geom" );
+
+	osg::Shader *VertObj = new osg::Shader( osg::Shader::VERTEX );
+	osg::Shader *FragObj = new osg::Shader( osg::Shader::FRAGMENT );
+	program->addShader( VertObj );
+	program->addShader( FragObj );
+
+	LoadShaderSource( VertObj , "glsl/building_geom.vert" );
+	LoadShaderSource( FragObj , "glsl/building_geom.frag" );
+
+	ss->setAttributeAndModes( program, osg::StateAttribute::ON );
+
+	//настроить uniform'ы
+	//SetupUniforms( ss );
+}
+
+void BuildingShaderPatchNode::LoadShaderSource( osg::Shader* shader, const std::string& fileName )
+{
+	// load source from a file.
+	std::string fqFileName = osgDB::findDataFile(fileName);
+	if( fqFileName.length() != 0 )
+	{
+		shader->loadShaderSourceFromFile( fqFileName.c_str() );
+	}
+	else
+	{
+		osg::notify(osg::WARN) << "File \"" << fileName << "\" not found." << std::endl;
+	}
+}
---
+++ FrustumTerrainShader/BuildingShader/BuildingShaderPatchNode.h
@@ -0,0 +1,38 @@
+#ifndef _BUILDING_SHADER_PATCH_NODE_H_
+#define _BUILDING_SHADER_PATCH_NODE_H_
+
+#include <osg/Group>
+#include <osg/Referenced>
+#include <osg/ref_ptr>
+#include <osg/Program>
+
+class BuildingShaderPatchNode : public osg::Referenced
+{
+public:
+	BuildingShaderPatchNode();
+	virtual ~BuildingShaderPatchNode();
+
+	//вернуть узел содержащий застройку
+	osg::ref_ptr< osg::Group > getRootNode() { return m_rootNode.get(); }
+
+private:
+	//инициализировать корневой узел застройки
+	void InitBuildingNode();
+
+	//добавить шейдер в узел
+	void AddShader();
+
+	//настроить uniform'ы
+	void SetupUniforms( osg::StateSet* ss );
+
+	// load source from a file.
+	void LoadShaderSource( osg::Shader* shader, const std::string& fileName );
+
+	//uniform для задания положения наблюдателя
+	osg::ref_ptr< osg::Uniform > m_unfVisPos;
+
+	//корневой узел
+	osg::ref_ptr< osg::Group > m_rootNode;
+};
+
+#endif	//_BUILDING_SHADER_PATCH_NODE_H_
