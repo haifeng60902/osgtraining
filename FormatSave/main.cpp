eea21837893c2952e380c4de453ececd7541e7a8
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2009-11-24 20:29:13 +0000

    попытка сохранить в какой нибудь 3d формат

--- FormatSave/main.cpp
+++ FormatSave/main.cpp
@@ -1,6 +1,32 @@
+#include <osgViewer/Viewer>
+#include <osgViewer/ViewerEventHandlers>
+#include <osgDB/ReadFile>
+#include <osgDB/WriteFile>
+
 #include <iostream>
 
 int main()
 {
-	std::cout << "ok\n";
+	// Create a Viewer.
+	osgViewer::Viewer viewer;
+
+	// add the stats handler
+	viewer.addEventHandler( new osgViewer::StatsHandler );
+
+	//	viewer.addEventHandler( new KeyboardHandler );
+
+	//настройка камеры
+	viewer.getCamera()->setProjectionMatrixAsPerspective( 45.0, 600.0/ 1024.0 , 1.0 , 35000.0 );
+
+	//добавить модель земной поверхности
+	osg::ref_ptr<osg::Node> root = osgDB::readNodeFile( "ball.osg" );
+
+	bool res = osgDB::writeNodeFile( *root , "test.3ds" ); 
+
+	viewer.setSceneData( root.get() );
+
+	viewer.getCamera()->setComputeNearFarMode(osg::CullSettings::DO_NOT_COMPUTE_NEAR_FAR); 
+
+	// Display, and main loop.
+	return viewer.run();
 }
