2322c080df46672b6e196ca6e3962f2a72b78455
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2009-11-26 22:32:03 +0000

    конвертор из xml в файл геометрии

---
+++ XMLTree2Geom/main.cpp
@@ -0,0 +1,41 @@
+#include "ticpp.h"
+
+#include <osgViewer/Viewer>
+#include <osgViewer/ViewerEventHandlers>
+ 
+int main()
+{
+
+
+	try
+	{
+		// Load a document
+		ticpp::Document doc( "tree.xml" );
+		doc.LoadFile();
+
+		// Get an element by chaining calls - no return values to check, no TiXmlHandle
+		ticpp::Element* pElem = dynamic_cast< ticpp::Element* >( doc.FirstChildElement()->NextSibling() );
+
+		// do something useful here
+	}
+	catch( ticpp::Exception& ex )
+	{
+		// If any function has an error, execution will enter here.
+		// Report the error
+		std::cout << ex.what();
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	// Create a Viewer.
+	osgViewer::Viewer viewer;
+
+	// add the stats handler
+	viewer.addEventHandler( new osgViewer::StatsHandler );
+
+	//viewer.setSceneData( tree->getRootNode().get() );
+
+	viewer.getCamera()->setComputeNearFarMode(osg::CullSettings::DO_NOT_COMPUTE_NEAR_FAR); 
+
+	// Display, and main loop.
+	return viewer.run();
+}
