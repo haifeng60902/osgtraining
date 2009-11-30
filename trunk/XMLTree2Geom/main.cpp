299a6b87347281ebd7815c29a5aa3ff4dcb0c76f
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2009-11-30 21:59:56 +0000

    попытка использовать tcpp

--- trunk/XMLTree2Geom/main.cpp
+++ trunk/XMLTree2Geom/main.cpp
@@ -7,22 +7,39 @@ int main()
 {
 
 
-	try
+//	try
 	{
 		// Load a document
 		ticpp::Document doc( "tree.xml" );
 		doc.LoadFile();
 
 		// Get an element by chaining calls - no return values to check, no TiXmlHandle
-		ticpp::Element* pElem = dynamic_cast< ticpp::Element* >( doc.FirstChildElement()->NextSibling() );
+		ticpp::Element* pElem = dynamic_cast< ticpp::Element* >( doc.FirstChildElement() );
+
+		ticpp::Iterator< ticpp::Element > child( "branch" );
+		for ( child = child.begin( doc.FirstChildElement() ); child != child.end(); child++ )
+		{
+			std::string sFile;
+			child->GetAttribute( "texture" , &sFile );
+			int a = 1;
+		}
+
+//////////////////////////////////////////////////////////////////////////
+		ticpp::Iterator< ticpp::Element > childF( "fronds" );
+		for ( childF = childF.begin( doc.FirstChildElement() ); childF != childF.end(); childF++ )
+		{
+			std::string sFile;
+			childF->GetAttribute( "texture" , &sFile );
+			int a = 1;
+		}
 
 		// do something useful here
 	}
-	catch( ticpp::Exception& ex )
+//	catch( ticpp::Exception& ex )
 	{
 		// If any function has an error, execution will enter here.
 		// Report the error
-		std::cout << ex.what();
+//		std::cout << ex.what();
 	}
 
 	//////////////////////////////////////////////////////////////////////////
