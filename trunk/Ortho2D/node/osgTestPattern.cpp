c0ceee067447b05e17b407140790d3b88fbd18ac
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2009-12-13 10:26:57 +0000

    пока не работает :(

--- trunk/Ortho2D/node/osgTestPattern.cpp
+++ trunk/Ortho2D/node/osgTestPattern.cpp
@@ -20,7 +20,7 @@ osgTestPattern::~osgTestPattern()
 void osgTestPattern::CreateGeom()
 {
 	//создать геометрию
-	osg::ref_ptr< osg::Geode > geode = new osg::Geode;
+	m_Geode = new osg::Geode;
 
 	osg::ref_ptr< osg::Geometry > geom = new osg::Geometry;
 
@@ -28,20 +28,29 @@ void osgTestPattern::CreateGeom()
 	osg::ref_ptr<osg::Vec3Array> v = new osg::Vec3Array;
 	geom->setVertexArray( v.get() );
 
-	v->push_back( osg::Vec3( 0 , 0 , 0 ) );
-	v->push_back( osg::Vec3( 100 , 0 , 0 ) );
-	v->push_back( osg::Vec3( 100 , 0 , 100 ) );
+	v->push_back( osg::Vec3( -140 , -140 , 0 ) );
+	v->push_back( osg::Vec3( 140 , -140 , 0 ) );
+	v->push_back( osg::Vec3( 140 , 140 , 0 ) );
 
+	v->push_back( osg::Vec3( -140 , 0 , -140 ) );
+	v->push_back( osg::Vec3( 140 , 0 , -140 ) );
+	v->push_back( osg::Vec3( 140 , 0 , 140 ) );
+	
 	// Create an array for the single normal.
 	osg::ref_ptr< osg::Vec4Array > c = new osg::Vec4Array;
 	geom->setColorBinding( osg::Geometry::BIND_PER_PRIMITIVE );
 	geom->setColorArray( c.get() );
 
 	c->push_back( osg::Vec4( 1,0,0,1) );
+	c->push_back( osg::Vec4( 1,1,0,1) );
 
 	geom->addPrimitiveSet( new osg::DrawArrays( osg::PrimitiveSet::TRIANGLES , 0, v->size() ) );
 
-	geode->addDrawable( geom.get() );
+	// Don't throw away single-vertex primitives.
+	osg::BoundingBox bbox( -1000, -1000, -1000, 1000, 1000, 1000 );
+	geom->setInitialBound( bbox );
 
-	m_Group->addChild( geode.get() );
+	m_Geode->addDrawable( geom.get() );
+
+	//m_Group->addChild( m_Geode.get() );
 }
