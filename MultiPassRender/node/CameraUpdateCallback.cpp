b3cecd1553e432d8abd2cb4d2797b1a37f65c8e0
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2010-01-12 22:54:58 +0000

    двигающаяся камера, показывающая объект с правильного ракурса

--- MultiPassRender/node/CameraUpdateCallback.cpp
+++ MultiPassRender/node/CameraUpdateCallback.cpp
@@ -1,5 +1,11 @@
 #include "CameraUpdateCallback.h"
 
+#include <osg/Camera>
+
+#include <iostream>
+
+float fY = 0.0f;
+
 CameraUpdateCallback::CameraUpdateCallback()
 {
 
@@ -7,6 +13,21 @@ CameraUpdateCallback::CameraUpdateCallback()
 
 void CameraUpdateCallback::operator()(osg::Node* node, osg::NodeVisitor* nv)
 {
+	osg::Camera* cam = dynamic_cast< osg::Camera* >( node );
+	if ( cam )
+	{
+		osg::Matrix mtTr , mtRtX , mtRtZ;
+		mtRtX.makeRotate( osg::DegreesToRadians( -90.0 ) , 1, 0 , 0 );
+		mtRtZ.makeRotate( osg::DegreesToRadians( 90.0 ) , 0, 0 , 1 );
+		mtTr.makeTranslate( 0 , 0 , fY );
+
+		fY -= 0.001;
+
+		std::cout << fY << " ";
+
+		//задать явно смещение
+		cam->setViewMatrix( mtRtZ * mtRtX * mtTr);
+	}
 
 	traverse(node,nv);
 }
