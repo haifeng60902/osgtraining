3d773e93087d5c86f064742334f1cee57e1cc008
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2010-01-12 22:54:58 +0000

    двигающаяся камера, показывающая объект с правильного ракурса

--- trunk/MultiPassRender/main.cpp
+++ trunk/MultiPassRender/main.cpp
@@ -22,32 +22,24 @@ int main()
 	//настройка камеры
 	viewer.getCamera()->setProjectionMatrixAsPerspective( 45.0, WIN_W / WIN_H , 1.0 , 35000.0 );
 
-	osg::ref_ptr< osg::Node > node = osgDB::readNodeFile( "flt/sphere/sphere.flt" );
+	//osg::ref_ptr< osg::Node > node = osgDB::readNodeFile( "flt/sphere/sphere.flt" );
+	osg::ref_ptr< osg::Node > node = osgDB::readNodeFile( "flt/mi17/MI_17_lod.flt" );
 	viewer.setSceneData( node.get() );
 
 	viewer.getCamera()->setComputeNearFarMode(osg::CullSettings::DO_NOT_COMPUTE_NEAR_FAR); 
 
 	float fY = 0.0f;
 
-	//viewer.getCamera()->setUpdateCallback( new CameraUpdateCallback() );
+	osg::Camera* cam = viewer.getCamera();
+	viewer.getCamera()->setUpdateCallback( new CameraUpdateCallback() );
 
 	// set up the camera manipulation with out custom manipultor
-	viewer.setCameraManipulator( new UfoManipulator() );
+	//viewer.setCameraManipulator( new UfoManipulator() );
 
 	// Display, and main loop.
 	while (!viewer.done())
 	{
-		osg::Matrix mtTr;
-		mtTr.makeRotate( osg::DegreesToRadians( -90.0 ) , 1, 0 , 0 );
-		
-		//задать явно смещение
-		//viewer.getCamera()->setViewMatrix( mtTr );
-
 		viewer.frame();
-
-		fY += 0.01;
-
-		//std::cout << fY << " ";
 	}
 
 	//viewer.run();
