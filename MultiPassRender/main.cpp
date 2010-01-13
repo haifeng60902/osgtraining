466da5c81cff55f1fdbfbe5f2a53cc78a35a01c2
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2010-01-13 15:29:22 +0000

    объект отображается в текстуру и выводится на четырехугольник

--- MultiPassRender/main.cpp
+++ MultiPassRender/main.cpp
@@ -1,6 +1,7 @@
 #include "binDef.h"
 #include "CameraUpdateCallback.h"
 #include "UfoManipulator.h"
+#include "osgTexturePlane.h"
 
 #include <osgViewer/Viewer>
 #include <osgViewer/ViewerEventHandlers>
@@ -23,24 +24,26 @@ int main()
 	viewer.getCamera()->setProjectionMatrixAsPerspective( 45.0, WIN_W / WIN_H , 1.0 , 35000.0 );
 
 	//osg::ref_ptr< osg::Node > node = osgDB::readNodeFile( "flt/sphere/sphere.flt" );
-	osg::ref_ptr< osg::Node > node = osgDB::readNodeFile( "flt/mi17/MI_17_lod.flt" );
-	viewer.setSceneData( node.get() );
+	//osg::ref_ptr< osg::Node > node = osgDB::readNodeFile( "flt/mi17/MI_17_lod.flt" );
+
+	//простая геометрия с текстурой
+	osgTexturePlane plane;
+	viewer.setSceneData( plane.GetNode().get() );
 
 	viewer.getCamera()->setComputeNearFarMode(osg::CullSettings::DO_NOT_COMPUTE_NEAR_FAR); 
 
 	float fY = 0.0f;
 
-	osg::Camera* cam = viewer.getCamera();
-	viewer.getCamera()->setUpdateCallback( new CameraUpdateCallback() );
+	//viewer.getCamera()->setUpdateCallback( new CameraUpdateCallback() );
 
 	// set up the camera manipulation with out custom manipultor
 	//viewer.setCameraManipulator( new UfoManipulator() );
 
 	// Display, and main loop.
-	while (!viewer.done())
-	{
-		viewer.frame();
-	}
+	//while (!viewer.done())
+	//{
+	//	viewer.frame();
+	//}
 
-	//viewer.run();
+	viewer.run();
 }
