5e0dc7a1e97acf571d0c6e9cb32bbf7e1840fd05
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2010-01-28 22:00:25 +0000

    первая рабочая версия. следующий раз буду добавлять шейдеры

--- MultiPassRender/MainNode/CameraPlane/CameraTexture0/UpdateCallbackCamera0.cpp
+++ MultiPassRender/MainNode/CameraPlane/CameraTexture0/UpdateCallbackCamera0.cpp
@@ -1,20 +1,15 @@
 #include "UpdateCallbackCamera0.h"
 
 #include "KeyboardState.h"
+#include "CameraState.h"
 
 #include <osg/Camera>
 
 #include <iostream>
 
-namespace
+UpdateCallbackCamera0::UpdateCallbackCamera0()
 {
-	float fY = 0.0f;
-}
 
-
-UpdateCallbackCamera0::UpdateCallbackCamera0() : m_fMoveSpeed( 0.0 )
-{
-	m_v3Pos = osg::Vec3( 0.0 , 0.0 , 0.0 );
 }
 
 void UpdateCallbackCamera0::operator()(osg::Node* node, osg::NodeVisitor* nv)
@@ -22,16 +17,13 @@ void UpdateCallbackCamera0::operator()(osg::Node* node, osg::NodeVisitor* nv)
 	osg::Camera* cam = dynamic_cast< osg::Camera* >( node );
 	if ( cam )
 	{
-		//обработать вращения
-		ProcessRotate();
-
-		//обработать перемещение
-		ProcessMove();
+		//получить доступ к состоянию камеры 
+		binCameraState &mCamState = CameraState::Instance().GetCameraState();
 
 		osg::Matrix mtTr , mtRtX , mtRtZ , mtRes;
-		mtRtX.makeRotate( osg::DegreesToRadians( m_v3Rot.x() - 90.0 ) , 1, 0 , 0 );
-		mtRtZ.makeRotate(  osg::DegreesToRadians( m_v3Rot.z() ) , 0, 0 , 1 );
-		mtTr.makeTranslate( m_v3Pos );	
+		mtRtX.makeRotate( osg::DegreesToRadians( mCamState.m_dH - 90.0 ) , 1, 0 , 0 );
+		mtRtZ.makeRotate(  osg::DegreesToRadians( mCamState.m_dR ) , 0, 0 , 1 );
+		mtTr.makeTranslate( mCamState.m_dX , mCamState.m_dY , mCamState.m_dZ );	
 
 		mtRes = mtTr * mtRtZ * mtRtX;
 
@@ -40,72 +32,4 @@ void UpdateCallbackCamera0::operator()(osg::Node* node, osg::NodeVisitor* nv)
 	}
 
 	traverse(node,nv);
-}
-
-void UpdateCallbackCamera0::ProcessRotate()
-{
-	//обработать вращения
-
-	//получить доступ к состоянию клавиатуры
-	binEvents &mEvents = KeyboardState::Instance().GetEvents();
-
-	m_v3Rot.z() = m_v3Rot.z() + mEvents.m_dX * 0.5;
-	m_v3Rot.x() = m_v3Rot.x() + mEvents.m_dY * 0.5;
-
-	//ограничение диапазона углов
-	if ( m_v3Rot.x() > 360.0 )
-		m_v3Rot.x() -= 360.0;
-	else
-		if ( m_v3Rot.x() < -360.0 )
-			m_v3Rot.x() += 360.0;
-
-	if ( m_v3Rot.z() > 360.0 )
-		m_v3Rot.z() -= 360.0;
-	else
-		if ( m_v3Rot.z() < -360.0 )
-			m_v3Rot.z() += 360.0;
-
-}
-
-void UpdateCallbackCamera0::ProcessMove()
-{
-	//обработать перемещение
-
-	//получить доступ к состоянию клавиатуры
-	binEvents &mEvents = KeyboardState::Instance().GetEvents();
-	if ( mEvents.m_bLeft )
-		//перемещение камеры вперед
-		MoveForward();
-	else
-		if ( mEvents.m_bRight )
-			//перемещение камеры назад
-			MoveBackward();
-}
-
-void UpdateCallbackCamera0::MoveForward()
-{
-	//перемещение камеры вперед
-	double dZ = cos( osg::DegreesToRadians( -m_v3Rot.x() + 90.0 ) );
-	double nD = sqrt( 1.0 - dZ * dZ );
-
-	double dX = -nD * sin( osg::DegreesToRadians( m_v3Rot.z() ) );
-	double dY = -nD * cos( osg::DegreesToRadians( m_v3Rot.z() ) );
-
-	m_v3Pos.x() += dX * 0.01;
-	m_v3Pos.y() += dY * 0.01;
-	m_v3Pos.z() += dZ * 0.01;
-}
-
-void UpdateCallbackCamera0::MoveBackward()
-{
-	//перемещение камеры назад
-	double dZ = -cos( osg::DegreesToRadians( -m_v3Rot.x() + 90.0 ) );
-	double nD = sqrt( 1.0 - dZ * dZ );
-
-	double dX = nD * sin( osg::DegreesToRadians( m_v3Rot.z() ) );
-	double dY = nD * cos( osg::DegreesToRadians( m_v3Rot.z() ) );
-
-	m_v3Pos.x() += dX * 0.01;
-	m_v3Pos.y() += dY * 0.01;
-	m_v3Pos.z() += dZ * 0.01;
 }
