735c03e669c8a61652bac6d87bd511f756573565
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2011-12-14 18:33:14 +0000

    тестовая структура

--- src/Test1/main.cpp
+++ src/Test1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include <windows.h>
 #include "DX11Renderer.h"
 
 int main()
@@ -7,4 +8,10 @@ int main()
 	DX11Renderer *dx11=new DX11Renderer();
 
 	std::cout << dx11->getBackendName() << "\n";
+
+	GetActiveWindow();
+
+	bool bRes=dx11->InitDevice(512,512);
+
+	delete dx11;
 }
--- src/render/dx11backend/DX11Renderer.cpp
+++ src/render/dx11backend/DX11Renderer.cpp
@@ -14,3 +14,33 @@ std::string DX11Renderer::getBackendName()
 {
 	return std::string("DX11Renderer");
 }
+
+bool DX11Renderer::InitDevice(int iX, int iY)
+{
+	//инициализация девайса
+	//инициализация девайса
+	HRESULT hr = S_OK;
+
+	UINT createDeviceFlags = 0;
+#ifdef _DEBUG
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	D3D_DRIVER_TYPE driverTypes[] =
+	{
+		D3D_DRIVER_TYPE_HARDWARE,
+		D3D_DRIVER_TYPE_WARP,
+		D3D_DRIVER_TYPE_REFERENCE,
+	};
+	UINT numDriverTypes = ARRAYSIZE( driverTypes );
+
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+	};
+	UINT numFeatureLevels = ARRAYSIZE( featureLevels );
+
+	return true;
+}
