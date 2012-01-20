c966e459eacff16dbd6ca5d27c057b37396ffe8b
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2012-01-20 09:53:35 +0000

    добавил лог

--- branches/scons/src/LogInfo/LogInfo.cpp
+++ branches/scons/src/LogInfo/LogInfo.cpp
@@ -1,9 +1,21 @@
 #include "LogInfo.h"
 
+#include <stdarg.h>
+#include <sstream>
+
 LogInfo::LogInfo()
 {
 #ifdef LOG2FILE
-	m_out.open( "info.log" , std::ios::out | std::ios::binary );
+	m_out.open("tmp/info.log", std::ios::out | std::ios::binary);
+#endif
+
+#ifdef LOG2FILE	|| LOG2CONSOLE
+	m_mLog[1]="ALERT";
+	m_mLog[8]="ERROR";
+	m_mLog[16]="WARNING";
+	m_mLog[64]="INFO";
+	m_mLog[128]="DEBUG";
+	m_mLog[256]="TRACE";
 #endif
 }
 
@@ -14,7 +26,7 @@ LogInfo::~LogInfo()
 #endif
 }
 
-void LogInfo::Print( std::string _str )
+void LogInfo::Print(std::string _str)
 {
 	//добавить сообщение в log
 #ifdef LOG2FILE	
@@ -24,4 +36,26 @@ void LogInfo::Print( std::string _str )
 #ifdef LOG2CONSOLE
 	std::cout << _str.c_str();
 #endif
+}
+
+void LogInfo::Print(const char *module, int level, const char *fmt, va_list args)
+{
+	//добавить сообщение в log
+#ifdef LOG2FILE	|| LOG2CONSOLE
+
+	char buffer [512];
+	sprintf(buffer,fmt, *((va_list*)args));
+	
+	std::stringstream ss;
+	ss<<buffer<<"\n";
+
+#ifdef LOG2FILE	
+	m_out<<module<<"\t"<<m_mLog[level]<<"\t"<<ss.str()<<"\n";
+#endif	//LOG2FILE
+
+#ifdef LOG2CONSOLE
+	std::cout<<module<<"\t"<<m_mLog[level]<<"\t"<<ss.str()<<"\n";
+#endif	//LOG2CONSOLE
+
+#endif	//LOG2FILE	|| LOG2CONSOLE
 }
--- branches/scons/src/LogInfo/LogInfo.h
+++ branches/scons/src/LogInfo/LogInfo.h
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <map>
 
 class LogInfo : public Singleton< LogInfo >
 {
@@ -16,12 +17,19 @@ public:
 	~LogInfo();
 
 	//добавить сообщение в log
-	void Print( std::string _str );
+	void Print(std::string _str);
+
+	//добавить сообщение в log
+	void Print(const char *module, int level, const char *fmt, va_list args);
 
 private:
 
 #ifdef LOG2FILE
 	std::ofstream m_out;
+#endif
+
+#ifdef LOG2FILE	|| LOG2CONSOLE
+	std::map<int, std::string> m_mLog;
 #endif
 };
 
