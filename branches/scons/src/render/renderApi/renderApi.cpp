6075121810f6c00aa5f9347a5b31ae752d5218f4
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2012-01-20 06:19:38 +0000

    продолжаем настройку

--- branches/scons/src/render/renderApi/renderApi.cpp
+++ branches/scons/src/render/renderApi/renderApi.cpp
@@ -5,5 +5,18 @@
 void renderApi::init(tUInt64 hWnd, int iWidth, int iHeight, bool bFullscreen, int iAdapter, int vSync,
 					 MSAA_ENUM eMsaa)
 {
-	std::cout<<"init() "<< (int)eMsaa<<"\n";
+	INT createDeviceFlags = 0;
+#if defined(_DEBUG) || defined(DEBUG)
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	// Order of feature levels to attempt to create
+	static const D3D_FEATURE_LEVEL featureLevels[] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_1, D3D_FEATURE_LEVEL_10_0 };
+
+	// Create device
+	HRESULT hr = D3D11CreateDevice(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags,
+		featureLevels, sizeof(featureLevels) / sizeof(D3D_FEATURE_LEVEL), D3D11_SDK_VERSION,
+		&device, &featureLevel, &deviceContext);
+
+	std::cout<<"ok";
 }
