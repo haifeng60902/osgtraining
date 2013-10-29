db6262180033fb6f6be474495c830f1c0ebabfa2
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2013-10-29 08:04:39 +0000

    wallet cracker

--- Billboards/constant.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CONSTANT_H_
-#define _CONSTANT_H_
-
-#define MAX_SIZE 1.0f
-#define MAX_VOLUME 5.0f
-#define NUM_QUADS 100
-#define ALT_OFFSET 10.f
-
-#endif	_CONSTANT_H_
--- LineIntersector/constant.h
+++
@@ -1,7 +0,0 @@
-#ifndef _CONSTANT_H_
-#define _CONSTANT_H_
-
-#define MAX_SIZE 10.0f
-#define NUM_LINES 1000
-
-#endif	//_CONSTANT_H_
