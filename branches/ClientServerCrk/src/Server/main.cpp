95417c0e9ed0fbd398654df8d045cd0480997782
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2013-11-01 14:58:53 +0000

    work in progress

--- branches/ClientServerCrk/src/Server/main.cpp
+++ branches/ClientServerCrk/src/Server/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-//#include "lua/lua.hpp"
 #include "core/Lua/Lua_vm.h"
+#include "tcp/tcpacceptor.h"
 
 int main()
 {
@@ -11,6 +11,8 @@ int main()
 	std::cout << "vol is " << lvm.get_double("vol") << "\n";
 	std::cout << "rho is " << lvm.get_int("rho") << "\n";
 	std::cout << "method is " << lvm.get_string("method").c_str() << "\n";
+
+	TCPAcceptor acceptor(9999,"127.0.0.1");
 
 	std::cout<<"ok\n";
 }
