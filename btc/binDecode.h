9d104d831586bdd6bff04e5c1eba3c8bebdcdd86
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2013-11-03 13:26:10 +0000

    work in progress

---
+++ btc/binDecode.h
@@ -0,0 +1,12 @@
+#ifndef _BIN_DECODE_H_
+#define _BIN_DECODE_H_
+
+#include <map>
+#include <vector>
+#include <string>
+
+typedef std::vector<unsigned char> tVecChar;
+typedef std::map<unsigned char, tVecChar> tMapDecode;
+typedef std::vector<std::string> tVecStr;
+
+#endif	//_BIN_DECODE_H_
