8b6432b78deb93e4d3cdd3961d09a0b0301f3d3c
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2013-11-04 09:17:28 +0000

    work in progress

---
+++ src/Core/tcp/TCPStream.cpp
@@ -0,0 +1,32 @@
+#include "Tcp/TCPStream.h"
+
+TCPStream::TCPStream(SOCKET msock, struct sockaddr_in* address):msgsock(msock)
+{
+	m_peerIP=std::string(inet_ntoa(address->sin_addr));
+	m_peerPort=ntohs(address->sin_port);
+}
+
+TCPStream::~TCPStream()
+{
+	closesocket(msgsock);
+}
+
+std::string TCPStream::getPeerIP()
+{
+	return m_peerIP;
+}
+
+int TCPStream::getPeerPort()
+{
+	return m_peerPort;
+}
+
+int TCPStream::receive(char* buffer, int len) 
+{
+	return recv(msgsock, buffer, len, 0);
+}
+
+int TCPStream::send(const char* buffer, int len) 
+{
+	return ::send(msgsock, buffer, len, 0);
+}
