18746e2d84bceab62d3dcfbc71f2e3a7d8a0b42b
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2013-11-05 14:23:40 +0000

    work in progress

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
+#include <string>
+
+#include <io.h>
+#include <fcntl.h>
 
 #include "Core/Lua/Lua_vm.h"
 #include "ServerLogic.h"
-//#include "Tcp/TCPAcceptor.h"
 
 std::string sAddress("127.0.0.1");
+std::wstring wRule;
+std::wstring wAutosave;
+std::wstring wSuccess;
+std::wstring wPhrase;
+
 int iPort=9999;
 
 int main(int argc, char **argv)
 {
+	//для отображения юникода в консоли
+	_setmode(_fileno(stdout), _O_TEXT);	//_O_WTEXT	_O_TEXT
+
 	//имя конфига
 	for(int i=1; i<argc; i++)
 	{
@@ -20,11 +31,41 @@ int main(int argc, char **argv)
 			iPort=lvm.get_int("port");
 			continue;
 		}
+		else if (strcmp(argv[i], "-rule") == 0)
+		{
+			i++; if (i >= argc) break;
+			std::string sConf = argv[i];
+			wRule=std::wstring(sConf.begin(), sConf.end());
+			continue;
+		}
+		else if (strcmp(argv[i], "-autosave") == 0)
+		{
+			i++; if (i >= argc) break;
+			std::string sSave = argv[i];
+			wAutosave=std::wstring(sSave.begin(), sSave.end());
+			continue;
+		}
+		else if (strcmp(argv[i], "-success") == 0)
+		{
+			i++; if (i >= argc) break;
+			std::string sConf = argv[i];
+			wSuccess=std::wstring(sConf.begin(), sConf.end());
+			continue;
+		}
+		else if (strcmp(argv[i], "-phrase") == 0)
+		{
+			i++; if (i >= argc) break;
+			std::string sConf = argv[i];
+			wPhrase=std::wstring(sConf.begin(), sConf.end());
+			continue;
+		}
+
 	}
 //////////////////////////////////////////////////////////////////////////
 	ServerLogic m_ServerLogic;
 
 	m_ServerLogic.Init(iPort, sAddress);
+	m_ServerLogic.InitPassLogic(wRule, wAutosave, wPhrase);
 	m_ServerLogic.Process();
 	m_ServerLogic.Close();
 }
