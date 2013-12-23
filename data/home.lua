2770cae8b0481bd992884140e72fe0bfeecd417c
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2013-12-23 09:30:14 +0000

    pool conf

---
+++ data/home.lua
@@ -0,0 +1,19 @@
+hosts=
+{
+	{
+		host="asus1215b-pc";
+		user="asus1215b";
+		pass="specyzx";
+		path="c:/Test";
+		exec="note.bat";
+		miner="start cgminer.exe --scrypt -u dbhec.7970h -p x --worksize 256 -g 2 --intensity 13";
+	},
+	{
+		host="asus1215b1-pc";
+		user="asus1215b";
+		pass="specyzx";
+		path="c:/Test";
+		exec="note.bat";
+		miner="start cgminer.exe --scrypt -u dbhec.7970w -p x --worksize 256 -g 2 --intensity 13";
+	}
+}
