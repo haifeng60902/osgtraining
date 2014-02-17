a3c6cf46102876b0faf825732623e3196998a8a0
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2014-02-17 07:36:29 +0000

    doge conf

--- branches/ClientServerCrk/data/server.bat
+++ branches/ClientServerCrk/data/server.bat
@@ -1,2 +1,2 @@
-..\bin\x86\vc100.release\server.exe -config server.conf x-phrase vdegyxy.txt -rule 7890ext.txt -autosave chain.txt
+..\bin\x86\vc110.release\server.exe -config server.conf x-phrase vdegyxy.txt -rule 7890ext.txt -autosave chain.txt -start_len 5
 pause
