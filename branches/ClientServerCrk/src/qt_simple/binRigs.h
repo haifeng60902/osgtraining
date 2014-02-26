0472fafab5651ccd0b669edf59316a254c52a714
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2014-02-26 12:20:49 +0000

    work in progress

---
+++ branches/ClientServerCrk/src/qt_simple/binRigs.h
@@ -0,0 +1,14 @@
+#ifndef _BIN_RIGS_H_
+#define _BIN_RIGS_H_
+
+#include <vector>
+#include <string>
+
+struct binRig
+{
+	std::string sRig;
+};
+
+typedef std::vector<binRig> tVecRig;
+
+#endif	//_BIN_RIGS_H_
---
+++ branches/ClientServerCrk/src/qt_simple/luaParseConf.h
@@ -0,0 +1,12 @@
+#ifndef _LUA_PARSE_CONF_H_
+#define _LUA_PARSE_CONF_H_
+
+class luaParseConf
+{
+public:
+	luaParseConf();
+	~luaParseConf();
+
+};
+
+#endif	//_LUA_PARSE_CONF_H_
