8fd42aead43458ec318c1e92b6d14ad300cd62dd
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2014-04-01 18:20:07 +0000

    reorganize conf

--- data/server.bat
+++ data/server.bat
@@ -1,2 +1,2 @@
-..\bin\x86\vc110.release\server.exe -config server.conf x-phrase vdegyxy.txt -rule 7890ext.txt -autosave chain.txt -start_len 5
+..\bin\x86\vc100.release\server.exe -config server.conf x-phrase vdegyxy.txt -rule doge.txt -autosave chain.txt -start_len 8
 pause
