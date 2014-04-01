bd8acb082014e87250a202f96e952056ff351f90
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2014-04-01 18:26:48 +0000

    work in progress qt

--- branches/ClientServerCrk/build_release64.bat
+++ branches/ClientServerCrk/build_release64.bat
@@ -1,4 +1,9 @@
-call scons Server arch=x86_64 mode=release %*
-call scons Client arch=x86_64 mode=release %*
-call scons Proxy arch=x86_64 mode=release %*
+rem call scons Server arch=x86_64 mode=release %*
+
+
+cd src\find
+start getQtMoc.bat
+cd ..\..
+call scons find arch=x86_64 mode=release %*
+
 pause
