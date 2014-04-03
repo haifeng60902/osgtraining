3c5bae712b5a30ea293d75c64021ebc6d6ae9d4d
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2014-04-03 08:24:17 +0000

    client 7970x2

--- branches/ClientServerCrk/data/configs/rigs/client.lua
+++ branches/ClientServerCrk/data/configs/rigs/client.lua
@@ -1,5 +1,5 @@
 systray=1;
-user="dbhec.7970w";
+user="dbhec.7970x2";
 pass="x";
 host="127.0.0.1";
 port=4028;
@@ -14,7 +14,7 @@ miners=
 	{
 		miner="exeminer.exe";
 		path="f:/Test/aida64/Language/exeminer/";
-		param="--gpu-powertune 20 --scan-time 1 --queue 4 --nscrypt --shaders 2048 --worksize 256 -g 2 --thread-concurrency 8192 --intensity 13 --gpu-memclock 1350 --gpu-engine 850 --api-listen --api-allow W:172.16.1/24 --socks-proxy 172.16.1.61:1063";
+		param="-d 0,1 --gpu-powertune 20 --scan-time 1 --queue 4 --nscrypt --shaders 2048 --worksize 256 -g 2 --thread-concurrency 8192 --intensity 12,13 --gpu-memclock 1350 --gpu-engine 850 --api-listen --api-allow W:127.0.0.1 --socks-proxy 172.16.1.61:1063";
 	},
 };
 
@@ -22,25 +22,25 @@ coins=
 {
 	{
 		coin="VTC(Vertcoin)";
-		miner="exeminer";
+		miner="exeminer.exe";
 		icon="icons/vertcoin-64x64.png";
 		pools={"stratum+tcp://vtc.poolz.net:3333","stratum+tcp://pool.verters.com:3333"};
 	},
 	{
 		coin="EXE(Execoin)";
-		miner="exeminer";
+		miner="exeminer.exe";
 		icon="icons/execoin-64x64.png";
 		pools={"stratum+tcp://exe.coinsprofit.com:3341","stratum+tcp://stratum-exe.poolz.net:3333"};
 	},
 	{
 		coin="CSC(Casinocoin)";
-		miner="cgminer";
+		miner="cgminer.exe";
 		icon="icons/casinocoin-64x64.png";
 		pools={"stratum+tcp://pool-eu.hash.so:1337","stratum+tcp://pool-us.hash.so:1337"};
 	},
 	{
 		coin="LTC(Litecoin)";
-		miner="cgminer";
+		miner="cgminer.exe";
 		icon="icons/litecoin-64x64.png";
 		pools={"stratum+tcp://united.wemineltc.com:3333","stratum+tcp://freedom.wemineltc.com:80"};
 	}
