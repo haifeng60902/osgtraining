cbac7464e6ac105689f5a14ddef5f2096df97eeb
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2014-04-12 13:10:52 +0000

    special for test purpose

---
+++ branches/ClientServerCrk/test/qt_test_server/main.cpp
@@ -0,0 +1,26 @@
+#include <string>
+
+#include "qt_rig.h"
+#include <QtWidgets/QApplication>
+
+std::string sConf("workers.lua");
+
+int main(int argc, char *argv[])
+{
+	//имя конфига
+	for(int i=1; i<argc; i++)
+	{
+		if (strcmp(argv[i], "-conf") == 0)
+		{
+			i++; if (i >= argc) break;
+			sConf=argv[i];
+			
+			continue;
+		}
+	}
+
+	QApplication a(argc, argv);
+	qt_rig w(sConf);
+	w.show();
+	return a.exec();
+}
