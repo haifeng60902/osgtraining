3cffad0d79e5ee94ea9dd1f1303857fa9f450617
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2014-04-12 14:54:12 +0000

    test client in progress

--- branches/ClientServerCrk/test/qt_test_client/qt_diag.h
+++ branches/ClientServerCrk/test/qt_test_client/qt_diag.h
@@ -17,29 +17,12 @@ class qt_diag: public QDialog
 
 private slots:
 	void timerTick();
-	void acceptConnection();
-	void updateServer();
-	void clientDisconnected();
 
 public:
 	qt_diag(QWidget *parent=NULL);
 	~qt_diag();
 
 private:
-	struct binClient
-	{
-		binClient():clientLabel(NULL),
-			iMsgSize(0),
-			iMsgClue(0),
-			iMsgRead(0)
-		{};
-		QLabel* clientLabel;
-		std::string sClient;
-		int iMsgSize;
-		int iMsgRead;
-		int iMsgClue;
-	};
-	typedef std::map<QTcpSocket*, binClient> tMapClient;
 
 	//fill string random char
 	void getRandStr(std::string* s);
@@ -47,10 +30,6 @@ private:
 	QTimer *timer;
 	QVBoxLayout* diagLayout;
 	QLabel* summaryLabel;
-
-	QTcpServer tcpServer;
-
-	tMapClient mClient;
 };
 
 #endif	//_QT_DIAG_H_
