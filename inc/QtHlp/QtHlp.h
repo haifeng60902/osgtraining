f8b7deacf65b3570f868c4abd418127ceaad6ac1
asmzx79 <asmzx79@3290fc28-3049-11de-8daa-cfecb5f7ff5b>
2014-04-17 18:15:47 +0000

    add new help str func

--- inc/QtHlp/QtHlp.h
+++ inc/QtHlp/QtHlp.h
@@ -1,6 +1,7 @@
 #ifndef _QT_HLP_H_
 #define _QT_HLP_H_
 
+#include <vector>
 #include <string>
 #include <QTcpSocket>
 
@@ -8,6 +9,9 @@ namespace QtHlp
 {
 	bool GetStr(QTcpSocket* sock, std::string* str, std::string* loc);
 	void WriteStr(QTcpSocket* sock, const std::string& s);
+
+	bool GetStrs(QTcpSocket* sock, std::vector<std::string>* vs);
+	void WriteStrs(QTcpSocket* sock, const std::vector<std::string>& vs);
 }
 
 #endif	//_QT_HLP_H_
